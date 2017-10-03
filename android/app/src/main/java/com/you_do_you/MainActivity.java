e4094a22721125dd0f1b276a371b4ba0482629ee
Emmanuel Fountoulakis <manos@Emmanuels-MacBook-Pro.local>
2017-10-03 12:48:15 -0400

    🚀

---
+++ android/app/src/main/java/com/you_do_you/MainActivity.java
@@ -0,0 +1,15 @@
+package com.you_do_you;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "you_do_you";
+    }
+}
